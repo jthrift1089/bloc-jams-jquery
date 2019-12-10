1a847a46c0e576658a3a6e639bdccb7cfe6aa0fa
justinbthrift@gmail.com <justinthrift@Justins-MBP.home>
2019-12-09 20:20:13 -0500

    Assignment 17 fixes

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -20,9 +20,22 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const prevSongIndex = currentSongIndex - 1;
-    if (prevSongIndex <= 0) { return; }
+    if (prevSongIndex <= -1) { return; }
     const prevSong = album.songs[prevSongIndex];
     player.playPause(prevSong);
   });
-  
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+  if (player.playState !== 'playing') { return; }
+  const currentTime = player.getTime();
+  const duration = player.getDuration();
+  const percent = (currentTime / duration) * 100;
+  $('#time-control .current-time').text( currentTime );
+  $('#time-control input').val(percent);
+  }, 1000);
+
 }
